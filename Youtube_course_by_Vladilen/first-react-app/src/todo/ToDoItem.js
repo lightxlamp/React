81fb33919afa9871b302f51d36a473360646d5d8
lightxlamp <sheveliukhin.s@gmail.com>
2020-09-08 00:54:23 +0600

    style: add styles to "ToDoItem"

--- Youtube_course_by_Vladilen/first-react-app/src/todo/ToDoItem.js
+++ Youtube_course_by_Vladilen/first-react-app/src/todo/ToDoItem.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const styles = {
+    li: {
+        display: 'flex', 
+        justifyContent: 'space-between',
+        alignItems: 'center', 
+        padding: '0.5rem 1rem',
+        border: '1px solid grey',
+        borderRadius: '4px',
+        marginBottom: '0.5rem'
+    },
+    input: {
+        marginRight: '1rem'
+    }
+}
+
 function ToDoItem({todo, index}) {
     return (    
-    <li>
-        <strong>{index + 1} </strong>
-        { todo.title }
+    <li style={styles.li}>
+        <span>
+            <input type="checkbox" style={styles.input} />
+            <strong>{index + 1} </strong>
+            { todo.title }
+        </span>
+        <button>&times;</button>
     </li>
     )  
 }
